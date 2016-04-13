77241461f8acd2f577344ca55439a00bd455aab5
James Garcia <james.garcia@us.sogeti.com>
2016-04-13 09:50:36 -0500

    Initial Commit

---
+++ public/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+// import {Hero} from './hero';
+// import {HeroDetailComponent} from './hero-detail.component';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: '/views/home.html'
+})
+export class AppComponent {
+}

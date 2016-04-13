305c69f0bfd908c0fd7f63a7c7cd27fe86f7d177
Andrew Feese <feese83@gmail.com>
2016-04-13 13:53:24 -0500

    fixing main.ts

--- public/app/main.ts
+++ public/app/main.ts
@@ -1,4 +1,10 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {FIREBASE_PROVIDERS, defaultFirebase, AngularFire} from 'angularfire2';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+	FIREBASE_PROVIDERS,
+	defaultFirebase('https://dazzling-inferno-8835.firebaseio.com'),
+	ROUTER_PROVIDERS
+]);

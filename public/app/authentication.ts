1c9e45bcdafaf94b967bf520f3e7e8e8175b4964
Andrew Feese <feese83@gmail.com>
2016-04-13 21:48:35 -0500

    moving authentication to a separate file

---
+++ public/app/authentication.ts
@@ -0,0 +1,23 @@
+import {Inject} from 'angular2/core';
+import {FirebaseAuth} from 'angularfire2';
+import {Observable} from 'rxjs/Observable';
+
+export class Authentication {
+	public login(email: string, password: string) {
+		this._auth.login({
+			email: email,
+			password: password
+		});
+	}
+
+	constructor(@Inject(FirebaseAuth) public _auth: FirebaseAuth) {
+		var firebase = new Firebase('https://dazzling-inferno-8835.firebaseio.com');
+
+		firebase.onAuth(function(authData) {
+			firebase.child('users').child(authData.uid).set({
+				provider: authData.provider,
+				name: authData.password.email.split('@')[0].replace('.','_')
+			})
+		});
+	}
+}

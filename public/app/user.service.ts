087cd3c7a3c1b1cd6d814b329e2d5df1f84fe592
Andrew Feese <feese83@gmail.com>
2016-04-21 08:32:19 -0500

    updating layout of search page

--- public/app/user.service.ts
+++ public/app/user.service.ts
@@ -13,18 +13,6 @@ export class UserService {
 		let email = username + '@us.sogeti.com';
 		return FirebaseRef.child('/users').orderByChild('email').startAt(email).endAt(email + "\uf8ff").once("value");
 	}
-	
-	public searchUsersByFullName(name: string){
-		name = name.toLowerCase();
-		var firstnameSearch = FirebaseRefUsers.orderByChild("fullname").startAt(name).endAt(name + "\uf8ff").once("value");
-		return  firstnameSearch;
-	}
-
-	public searchUsersByLastName(name: string){
-		name = name.toLowerCase();
-		var lastnameSearch = FirebaseRefUsers.orderByChild("last").startAt(name).endAt( name+ "\uf8ff").once("value") ;
-		return  lastnameSearch;
-	}
 
 	public searchBioByUID(uid: string){
 		return FirebaseRef.child('/bio').child(uid).once("value");

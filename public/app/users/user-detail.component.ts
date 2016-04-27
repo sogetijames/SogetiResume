4b7fdb4afa16d93e8864370354459e8189d440de
Andrew Feese <feese83@gmail.com>
2016-04-27 14:33:08 -0500

    moving firebase get/save logic to user service

--- public/app/users/user-detail.component.ts
+++ public/app/users/user-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {NgClass} from 'angular2/common';
 import {Router, RouteParams} from 'angular2/router';
 import {CurrentUser, FirebaseRef, FirebaseData} from '../shared/shared';
@@ -16,9 +16,8 @@ import {UserService} from './user.service';
 		ValuesPipe
 	]
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
 	editable: boolean;
-	uid: string;
 	user: any;
 	userCopy: any;
 	sortSkillsNameAZ: boolean;
@@ -27,14 +26,7 @@ export class UserDetailComponent {
 	showSkillProficiencyArrow: boolean;
 	openSection: string;
 
-	constructor(
-		private _currentUser: CurrentUser,
-		private _userService: UserService, 
-		private _routeParams: RouteParams,
-		private _router: Router,
-		private _valuesPipe: ValuesPipe,
-		private _firebaseData: FirebaseData
-	) {
+	ngOnInit() {
 		this.editable = false;
 		this.openSection = 'Bio';
 
@@ -44,32 +36,29 @@ export class UserDetailComponent {
 		this.showSkillProficiencyArrow = false;
 
 		let username = this._routeParams.get('username');
-		this._userService.getUserByUsername(username.replace('_', '.')).then( user => {
-			let data = user.val();
-
-			this.uid = Object.keys(data)[0];
-
-			this.user = data[this.uid];
-			this.user.profileImageURL = this.user.profileImageURL.replace('http://', 'https://') + "?s=250";
-			this.user.projects = [];
-			this.user.skills = [];
-			this.user.educations = [];
-
-			this.getUserProjects();
-			this.getUserSkills();
-			this.getUserEducations();			
+		this._userService.getUserDetails(username, (userObject) => {
+			this.user = userObject;
+			this.userCopy = Object.assign({}, this.user);
 		});
-	} 
+	}
+
+	constructor(
+		private _currentUser: CurrentUser,
+		private _userService: UserService, 
+		private _routeParams: RouteParams,
+		private _router: Router,
+		private _valuesPipe: ValuesPipe,
+		private _firebaseData: FirebaseData) { } 
 
 	clickEdit() {
 		this.editable = !this.editable;
 	}
 
 	clickSave() {
-		this.saveUserInfo();
-		this.saveUserProjects();
-		this.saveUserSkills();
-		this.saveUserEducations();
+		this._userService.saveUserEducations(this.user.uid, this.user.educations);
+		this._userService.saveUserProjects(this.user.uid, this.user.projects);
+		this._userService.saveUserSkills(this.user.uid, this.user.skills);
+		this._userService.saveUserInfo(this.user.uid, this.user);
 
 		this.userCopy = $.extend(true, {}, this.user);
 		this.editable = !this.editable;
@@ -126,125 +115,6 @@ export class UserDetailComponent {
 		} else {
 			project.skills.splice(index, 1);
 		}
-	}
-
-	private getUserProjects() {
-		this._userService.getDataForUidOnce('projects', this.uid).then( projects => {
-			let projectsObject = projects.val();
-
-			if (projectsObject != undefined) {
-				this.user.projects = projects.val();
-			}	
-					
-			this.userCopy = $.extend(true, {}, this.user);
-		});
-	}
-
-	private getUserSkills() {
-		this._userService.getDataForUidOnce('skills', this.uid).then( skills => {
-			let skillsObject = skills.val();
-
-			if (skillsObject != undefined) {
-				Object.keys(skillsObject).forEach((key) => {
-					this.user.skills.push({
-						key: key,
-						value: skillsObject[key]
-					});
-				});				
-			}
-
-			this.userCopy = $.extend(true, {}, this.user);
-		});
-	}
-
-	private getUserEducations() {
-		this._userService.getDataForUidOnce('educations', this.uid).then( educations => {
-			let educationsObject = educations.val();
-
-			if (educationsObject != undefined){
-				Object.keys(educationsObject).forEach((key) => {
-					this.user.educations.push({
-						title: key,
-						description: educationsObject[key]
-					});
-				});
-			}
-
-			this.userCopy = $.extend(true, {}, this.user);		
-		});
-	}
-
-	private saveUserInfo() {
-		let userRef = FirebaseRef.child('users').child(this.uid);
-		let userObj = {};
-
-		userObj['bio'] = this.user.bio;
-		userObj['first'] = this.user.first;
-		userObj['last'] = this.user.last;
-		userObj['title'] = this.user.title;
-		userObj['unit'] = this.user.unit;
-		userObj['practice'] = this.user.practice;
-		userObj['status/text'] = this.user.status.text;
-		if (this.user.status.text == 'ATO') {
-			this.user.status.description = '';
-		} 
-		userObj['status/description'] = this.user.status.description;
-
-		userRef.update(userObj, (error) => {
-			if (error) {
-				toastr.error('Error saving Info!', error);
-			} else {
-				toastr.success('Info Saved Successfully!');
-			}
-		});
-	}
-
-	private saveUserProjects() {
-		let projectsRef = FirebaseRef.child('projects').child(this.uid);
-
-		projectsRef.set(this.user.projects, (error) => {
-			if (error) {
-				toastr.error('Error saving Projects!', error);
-			} else {
-				toastr.success('Projects Saved Successfully!');
-			}
-		});
-	}
-
-	private saveUserSkills() {
-		let skillsObject = {};
-		let skillsRef = FirebaseRef.child('skills').child(this.uid);
-		let skills = this.user.skills;
-
-		for (var i = 0; i < skills.length; i++) {
-			skillsObject[skills[i].key] = skills[i].value;
-		}
-
-		skillsRef.set(skillsObject, (error) => {
-			if (error) {
-				toastr.error('Error saving Skills!', error);
-			} else {
-				toastr.success('Skills Saved Successfully!');
-			}
-		});
-	}
-
-	private saveUserEducations() {
-		let educationObject = {};
-		let educationRef = FirebaseRef.child('educations').child(this.uid);
-		let educations = this.user.educations;
-
-		for (var i = 0; i < educations.length; i++) {
-			educationObject[educations[i].title] = educations[i].description;
-		}
-
-		educationRef.set(educationObject, (error) => {
-			if (error) {
-				toastr.error('Error saving Education!', error);
-			} else {
-				toastr.success('Education Saved Successfully!');
-			}
-		});
 	}
 
 	private dynamicSort(property) {
--- public/app/users/user.service.ts
+++ public/app/users/user.service.ts
@@ -3,18 +3,86 @@ import {FirebaseRef} from '../shared/shared';
 
 @Injectable()
 export class UserService {
-	constructor() { }
+	private infoEditablePropertiesArray: string[];
+
+	constructor() { 
+		this.infoEditablePropertiesArray = ['bio', 'first', 'last', 'title', 'unit', 'practice', 'status'];
+	}
 
 	public getUserByUid(uid: string) {
 		return FirebaseRef.child('users').child(uid).once('value');
 	}
-	
-	public getUserByUsername(username: string) {
-		let email = username.toLowerCase() + '@us.sogeti.com';
-		return FirebaseRef.child('users').orderByChild('email').startAt(email).endAt(email + '\uf8ff').once('value');
-	}
 
 	public getDataForUidOnce(child: string, uid: string) {
 		return FirebaseRef.child(child).child(uid).once('value');
+	}
+
+	public getUserDetails(username: string, callback: Function) {
+		this.getUserByUsername(username).then((user) => {
+			let data = user.val();
+
+			let uid = Object.keys(data)[0];
+			let userObject = data[uid];
+
+			userObject.uid = uid;
+			userObject.profileImageURL = userObject.profileImageURL.replace('http://', 'https://') + "?s=250";
+			userObject.educations = [];
+			userObject.projects = [];
+			userObject.skills = [];
+
+			let educationsPromise = this.getDataForUidOnce('educations', uid);
+			let projectsPromise = this.getDataForUidOnce('projects', uid);
+			let skillsPromise = this.getDataForUidOnce('skills', uid);
+
+			Promise.all([educationsPromise, projectsPromise, skillsPromise]).then((dataSnapshot) => {
+				let educationsObject = dataSnapshot[0].val();
+				if (educationsObject != undefined) {
+					userObject.educations = educationsObject;
+				}
+
+				let projectsObject = dataSnapshot[1].val();
+				if (projectsObject != undefined) {
+					userObject.projects = projectsObject;
+				}
+
+				let skillsObject = dataSnapshot[2].val();
+				if (skillsObject != undefined) {
+					userObject.skills = skillsObject;
+				}
+
+				callback(userObject);
+			});
+		});
+	}
+
+	public saveUserEducations(uid: string, educations: any) {
+		FirebaseRef.child('educations').child(uid).set(educations, error => this.showAlert(error, 'Education') );
+	}
+
+	public saveUserProjects(uid: string, projects: any) {
+		FirebaseRef.child('projects').child(uid).set(projects, error => this.showAlert(error, 'Projects') );
+	}
+
+	public saveUserSkills(uid: string, skills: any) {
+		FirebaseRef.child('skills').child(uid).set(skills, error => this.showAlert(error, 'Skills') );
+	}
+
+	public saveUserInfo(uid: string, info: any) {
+		let infoObject = {};
+		this.infoEditablePropertiesArray.forEach( property => infoObject[property] = info[property] );
+		FirebaseRef.child('users').child(uid).update(infoObject, error => this.showAlert(error, 'Profile') );
+	}
+
+	private showAlert(error: any, property: string) {
+		if (error) {
+			toastr.error('Error Saving ' + property + '!', error);
+		} else {
+			toastr.success(property + ' Saved Successfully!');
+		}
+	}
+	
+	private getUserByUsername(username: string) {
+		let email = username.toLowerCase().replace('_', '.') + '@us.sogeti.com';
+		return FirebaseRef.child('users').orderByChild('email').startAt(email).endAt(email + '\uf8ff').once('value');
 	}
 }
